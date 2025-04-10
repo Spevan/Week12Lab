00a4d82d799b317db451f386d9fdf51705296b4d
Spevan <67030925+Spevan@users.noreply.github.com>
2025-04-10 15:06:40 -0400

    Network & Data Sync

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,33 @@
+using Unity.Netcode;
+using Unity.Netcode.Components;
 using UnityEngine;
-public class PlayerController : MonoBehaviour 
+public class PlayerController : NetworkBehaviour
 { 
-    public float speed = 5f; 
+    public float speed = 5f;
+
+    private void OnNetworkInstantiate()
+    {
+        if (!IsOwner)
+        {
+            
+        }
+    }
+
     void Update() 
     { 
+        if (!IsOwner)
+        {
+            return;
+        }
+
         float moveHorizontal = Input.GetAxis("Horizontal"); 
         float moveVertical = Input.GetAxis("Vertical"); 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); 
         transform.Translate(movement * speed * Time.deltaTime); 
-    } 
+    }
+
+    void LateUpdate() 
+    { 
+        Debug.Log("Player position: " + transform.position); 
+    }
 }
